d19015f58f78782682954456f2c4950698f0581b
pavlogurgu <pavlo.gurgu@gmail.com>
2022-11-07 20:43:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
   </StrictMode>
