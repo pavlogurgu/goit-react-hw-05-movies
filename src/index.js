a4d4af1d8f8dd69beb2c6d1331858dcbfcd8ea4d
pavlogurgu <pavlo.gurgu@gmail.com>
2022-11-07 20:50:42 +0200

    hw5-remarks

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </StrictMode>
